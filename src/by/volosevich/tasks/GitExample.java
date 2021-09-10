a1bf30433ee22e93b326f57ddd34d56ce2df0b40
Sergei Volosevich <sergey.volosevich93@gmail.com>
2021-09-10 21:58:52 +0200

    third commit

--- src/by/volosevich/tasks/GitExample.java
+++ src/by/volosevich/tasks/GitExample.java
@@ -2,8 +2,8 @@ package by.volosevich.tasks;
 
 public class GitExample {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello World dshagjkfdgklhkldfggfhsadvjgfjdhjldfgh");
+        System.out.println("Hello Worldfjdgjdfklhjkldfgjdflkmh");
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
