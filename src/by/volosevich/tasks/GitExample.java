8b559e5d5b0dfcfa3485130ffc2750a3f5d00a11
Sergei Volosevich <sergey.volosevich93@gmail.com>
2021-09-10 21:54:11 +0200

    second commit

--- src/by/volosevich/tasks/GitExample.java
+++ src/by/volosevich/tasks/GitExample.java
@@ -3,5 +3,14 @@ package by.volosevich.tasks;
 public class GitExample {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
