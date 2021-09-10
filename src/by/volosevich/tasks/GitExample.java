20a923a027b6a348b9a3fb28d61f2f84b9626e3a
Sergei Volosevich <sergey.volosevich93@gmail.com>
2021-09-10 22:01:21 +0200

    third commit

--- src/by/volosevich/tasks/GitExample.java
+++ src/by/volosevich/tasks/GitExample.java
@@ -1,16 +1,23 @@
 package by.volosevich.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GitExample {
     public static void main(String[] args) {
         System.out.println("Hello World dshagjkfdgklhkldfggfhsadvjgfjdhjldfgh");
-        System.out.println("Hello Worldfjdgjdfklhjkldfgjdflkmh");
+        Random random = new Random();
+        System.out.println(random.nextInt(10));
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        List<String> stringList = new ArrayList<>();
+        stringList.add("bfdshkgjdflkhgkdfn");
+        for (String s : stringList) {
+            System.out.println(s);
+        }
     }
 }
